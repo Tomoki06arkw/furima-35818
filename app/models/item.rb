5e782d90ef42514908723d8e6c4942a58bba47ae
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-06-10 17:09:36 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,11 +13,13 @@ class Item < ApplicationRecord
       validates :product_name
       validates :description
       validates :price
-      validates :category_id
-      validates :condition_id
-      validates :fee_id
-      validates :area_id
-      validates :delivery_day_id
+      with_options numericality: { other_than: 1 } do
+        validates :category_id
+        validates :condition_id
+        validates :fee_id
+        validates :area_id
+        validates :delivery_day_id
+      end
     end
 
 end
