17d6a8d9c288844c88a736d01f63b291cde2b055
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-06-10 15:17:06 +0900

    アクティブストレージ導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,14 @@ class ItemsController < ApplicationController
   # before_action :authenticate_user!, except: [:index]
   def index
   end
+  def new
+  end
+  def create
+  end
+
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:image)
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+    belongs_to :user
+    has_one_attached :image
+
+    with_options presence: true do
+      validates :product_name
+      validates :description
+      validates :price
+      validates :category_id
+      validates :condition_id
+      validates :fee_id
+      validates :area_id
+      validates :delivery_day_id
+    end
+
 end
