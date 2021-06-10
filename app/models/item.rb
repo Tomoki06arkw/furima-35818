ab63c69eea85e77c9ce83e975584a723c462c02f
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-06-10 16:55:11 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,13 @@
 class Item < ApplicationRecord
     belongs_to :user
     has_one_attached :image
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :area
+    belongs_to :category
+    belongs_to :condition
+    belongs_to :delivery_day
+    belongs_to :fee
+
 
     with_options presence: true do
       validates :product_name
