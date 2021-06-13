664f5dca2ff44e27159ad6614987eab28e108e0a
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-06-13 17:44:04 +0900

    Revert "削除ボタンとアクション定義"
    This reverts commit 70675461e7143d7daf0c3ad22aa82380284da2a2.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :set_user_id, only: [:edit, :update]
 
     def index
@@ -35,9 +35,7 @@ class ItemsController < ApplicationController
     end
 
     def destroy
-      if @item.destroy
-        redirect_to root_path
-      end
+      
     end
 
     private
