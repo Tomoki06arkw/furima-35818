223432ad883919d443edda9137a16fbc398e77e3
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-06-13 16:12:30 +0900

    エラーメッセージ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  
 
     def index
       @items = Item.all.order("created_at DESC")
@@ -24,8 +25,19 @@ class ItemsController < ApplicationController
 
     def edit
       @item = Item.find(params[:id])
+      unless @item.user_id == current_user.id
+        redirect_to action: :index
+      end
     end
-
+    
+    def update
+      @item = Item.find(params[:id])
+      if @item.update(item_params)
+        redirect_to root_path
+      else
+        render :edit
+      end
+    end
 
     private
 
