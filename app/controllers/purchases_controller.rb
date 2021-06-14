8b90fd98491921a24a3afee782d202f09df5b7d4
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-06-14 17:52:29 +0900

    購入のためのフォームを実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,4 +2,9 @@ class PurchasesController < ApplicationController
   def index
     @purchase_shipping_address = PurchaseShippingAddress.new
   end
+
+  def create
+    
+  end
+
 end
--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -1,22 +1,22 @@
 class PurchaseShippingAddress
 
-  include ActiveModel::module 
+  include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id
 
     with_options presence: true do
       validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hypen(-)"}
-      validates :prefecture_id, numecarilly: {other_than: 0, message: "can't be blank"}
+      validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
       validates :city
       validates :address
       validates :phone_number
       validates :user_id
       validates :item_id
     end
-
+    
     def save
       perchase = Perchase.create(user_id: user_id, item_id: item_id)
       ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, phone_number: phone_number, purchase_id: purchase.id)
     end
-  end
+  
 end
 
