011e6640e58edcaa385c59753546452242a6049f
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-06-14 23:08:26 +0900

    フォームの情報を保存できる。

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,7 +4,17 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    
+    @purchase_shipping_address = PurchaseShippingAddress.new(purchase_params)
+    if @purchase_shipping_address.valid?
+      @purchase_shipping_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
+  private
+  def purchase_params
+  params.require(:purchase_shipping_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -1,7 +1,7 @@
 class PurchaseShippingAddress
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id
 
     with_options presence: true do
       validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hypen(-)"}
@@ -14,7 +14,7 @@ class PurchaseShippingAddress
     end
     
     def save
-      perchase = Perchase.create(user_id: user_id, item_id: item_id)
+      purchase = Purchase.create(user_id: user_id, item_id: item_id)
       ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, phone_number: phone_number, purchase_id: purchase.id)
     end
   
