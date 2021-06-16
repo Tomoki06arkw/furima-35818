e56247eb9a31506ee27223df2676a7e24dab5837
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-06-16 19:26:37 +0900

    トークンをバリデーションに設定する

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -1,7 +1,7 @@
 class PurchaseShippingAddress
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id, :token
 
     with_options presence: true do
       validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hypen(-)"}
@@ -11,6 +11,7 @@ class PurchaseShippingAddress
       validates :phone_number, format: {with: /\A0[5789]0[-]?\d{4}[-]?\d{4}\z/, message: "Phone number is invalid"}
       validates :user_id
       validates :item_id
+      validates :token
     end
     
     def save
