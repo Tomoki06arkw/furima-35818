44c5cb5630d848f222dc509525a501a15ba7092f
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-06-17 18:46:19 +0900

    修正しました。

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,12 @@
 class PurchasesController < ApplicationController
 before_action :authenticate_user!, only: [:index]
 before_action :set_item, only: [:index, :create]
+before_action :set_id, only: [:index, :create]
 
   def index
     @purchase_shipping_address = PurchaseShippingAddress.new
 
-    if current_user.id == @item.user_id ||  @item.purchase == ''
-      redirect_to root_path
-    end
+    
   end
 
   def create
@@ -23,7 +22,7 @@ before_action :set_item, only: [:index, :create]
 
   private
   def purchase_params
-  params.require(:purchase_shipping_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:purchase_shipping_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
@@ -38,7 +37,12 @@ before_action :set_item, only: [:index, :create]
       currency: 'jpy'
     )
   end
-
+  
+  def set_id
+    if current_user.id == @item.user_id ||  @item.purchase == ''
+      redirect_to root_path
+    end
+  end
   
 end
 
